762dddc10d717f0cd3fb964b5145d46ce31f94a3
AndreiAfrasinei <andrei.afrasinei@assist.ro>
2019-01-28 16:30:17 +0200

    [Task-27277] Add Scaffold for entities (#2)

---
+++ app/controllers/entities_controller.rb
@@ -0,0 +1,74 @@
+class EntitiesController < ApplicationController
+  before_action :set_entity, only: [:show, :edit, :update, :destroy]
+
+  # GET /entities
+  # GET /entities.json
+  def index
+    @entities = Entity.all
+  end
+
+  # GET /entities/1
+  # GET /entities/1.json
+  def show
+  end
+
+  # GET /entities/new
+  def new
+    @entity = Entity.new
+  end
+
+  # GET /entities/1/edit
+  def edit
+  end
+
+  # POST /entities
+  # POST /entities.json
+  def create
+    @entity = Entity.new(entity_params)
+
+    respond_to do |format|
+      if @entity.save
+        format.html { redirect_to @entity, notice: 'Entity was successfully created.' }
+        format.json { render :show, status: :created, location: @entity }
+      else
+        format.html { render :new }
+        format.json { render json: @entity.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /entities/1
+  # PATCH/PUT /entities/1.json
+  def update
+    respond_to do |format|
+      if @entity.update(entity_params)
+        format.html { redirect_to @entity, notice: 'Entity was successfully updated.' }
+        format.json { render :show, status: :ok, location: @entity }
+      else
+        format.html { render :edit }
+        format.json { render json: @entity.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /entities/1
+  # DELETE /entities/1.json
+  def destroy
+    @entity.destroy
+    respond_to do |format|
+      format.html { redirect_to entities_url, notice: 'Entity was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_entity
+      @entity = Entity.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def entity_params
+      params.require(:entity).permit(:uid, :name, :description, :state, :extra_data)
+    end
+end
