e6687c01083811ce499a589c98771f43336dc2d6
Petru Cioata <petru.cioata@assist.ro>
2019-02-05 10:01:03 +0200

    [TASK-27487] Routes to deletedEntities only for index and show

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :entities
-  resources :deleted_entities
+  resources :deleted_entities, only: [:index, :show]
   root to: "entities#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
