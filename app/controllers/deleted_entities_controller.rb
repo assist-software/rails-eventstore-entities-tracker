c021a3f241b647942ce7532624f183e6eb6ec74a
Andrei Afrasinei <andrei.afrasinei@assist.ro>
2019-02-06 15:31:36 +0200

    [Task-27516] Add restore entity method in deleted entities controller

--- app/controllers/deleted_entities_controller.rb
+++ app/controllers/deleted_entities_controller.rb
@@ -1,11 +1,20 @@
 class DeletedEntitiesController < ApplicationController
-  before_action :set_deleted_entity, only: [:show]
+  before_action :set_deleted_entity, only: [:show, :restore]
+
+  include Commands::Execute
 
   def index
-    @deleted_entities = read_stream("DeletedEntities").to_a
+    @deleted_entities = read_stream("DeletedEntities").backward.to_a.uniq { |en| en.data[:uid]}
   end
 
   def show
+  end
+
+  def restore
+    cmd = Commands::Entities::RestoreEntity.new(uid: @deleted_entity.last.data[:uid])
+    execute(cmd)
+
+    redirect_to "/entities"
   end
 
   private
